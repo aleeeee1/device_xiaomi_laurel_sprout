d4eea347d799d3d0b85a3c7a1850e2df6a9178ee
aleeeee1 <pace.alessandro05@protonmail.com>
2023-12-30 14:14:56 +0000

    device.mk: added keybaster and gatekeeper

--- device.mk
+++ device.mk
@@ -288,6 +288,8 @@ PRODUCT_COPY_FILES += \
 
 # KEYSTORE
 PRODUCT_PACKAGES += \
+    android.hardware.gatekeeper@1.0.vendor \
+    android.hardware.keymaster@4.0.vendor \
     android.system.keystore2
 
 # Lights
