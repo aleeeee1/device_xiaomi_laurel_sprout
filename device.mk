6ae4ef03a011b45fd8d3676c7ba8d7ad9d0932dc
aleeeee1 <pace.alessandro05@protonmail.com>
2024-01-06 11:08:38 +0000

    laurel_sprout: downgrade android.hardware.audio to 7.0
    fixes:
    Cmdline: /vendor/bin/hw/android.hardware.audio.service
    pid: 3870, tid: 3876, name: HwBinder:3870_1  >>> /vendor/bin/hw/android.hardware.audio.service <<<
    uid: 1041
    signal 11 (SIGSEGV), code 1 (SEGV_MAPERR), fault addr 0x00000004
    Cause: null pointer dereference

--- device.mk
+++ device.mk
@@ -42,7 +42,7 @@ PRODUCT_OTA_ENFORCE_VINTF_KERNEL_REQUIREMENTS := true
 PRODUCT_PACKAGES += \
     android.hardware.audio@6.0-impl \
     android.hardware.audio.effect@6.0-impl \
-    android.hardware.audio@7.1-impl \
+    android.hardware.audio@7.0-impl \
     android.hardware.audio.effect@7.0-impl \
     android.hardware.audio.service \
     android.hardware.audio.sounddose-vendor-impl \
