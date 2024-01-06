5cb6d3deed00dd3cc5958cb555b376723ee9d799
aleeeee1 <pace.alessandro05@protonmail.com>
2024-01-06 02:34:23 +0000

    laurel_sprout: Build android.hardware.biometrics.fingerprint@2.1
    Control message: Could not find 'android.hardware.biometrics.fingerprint@2.1::IBiometricsFingerprint/default' for ctl.interface_start from pid: 519 (/system/bin/hwservicemanager)

--- device.mk
+++ device.mk
@@ -195,7 +195,8 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/updatable_apex.mk)
 # Fingerprint feature
 PRODUCT_PACKAGES += \
     android.hardware.biometrics.fingerprint@2.3-service.laurel_sprout \
-    vendor.xiaomi.hardware.fingerprintextension@1.0.vendor
+    vendor.xiaomi.hardware.fingerprintextension@1.0.vendor \
+    android.hardware.biometrics.fingerprint@2.1
 
 # FM
 PRODUCT_PACKAGES += \
