c561228edc61cd0e69cadadd471284af8e8e4340
aleeeee1 <pace.alessandro05@protonmail.com>
2024-01-30 11:57:27 +0000

    laurel_sprout: build a2dp-service

--- device.mk
+++ device.mk
@@ -112,6 +112,7 @@ PRODUCT_PACKAGES += \
     android.hardware.bluetooth.audio-impl \
     android.hardware.bluetooth.a2dp-impl \
     android.hardware.bluetooth.a2dp@1.0-impl \
+    android.hardware.bluetooth.a2dp@1.0-service \
     android.hardware.bluetooth.a2dp@1.0.vendor \
     android.hardware.bluetooth.audio@2.0-impl \
     android.hardware.bluetooth@1.0.vendor \
