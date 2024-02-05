62ed08be7f581da3eb14142948c14e6a8bb462ae
aleeeee1 <pace.alessandro05@protonmail.com>
2024-02-05 20:09:24 +0100

    laurel_sprout: added android.hardware.authsecret.IAuthSecret to manifest
    servicemanager: Could not find android.hardware.authsecret.IAuthSecret/default in the VINTF manifest.

--- device.mk
+++ device.mk
@@ -38,6 +38,9 @@ TARGET_EXCLUDES_AUDIOFX := true
 
 PRODUCT_OTA_ENFORCE_VINTF_KERNEL_REQUIREMENTS := true
 
+# AuthSecret
+PRODUCT_PACKAGES += \
+    android.hardware.authsecret@1.0-service
 # Audio
 PRODUCT_PACKAGES += \
     android.hardware.audio@7.0-impl \
