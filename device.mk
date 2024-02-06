06b51b45b9e9e3d6cf4a503d67eb73f5478e4522
aleeeee1 <pace.alessandro05@protonmail.com>
2024-02-06 10:58:38 +0100

    laurel_sprout: built android.hardware.vibrator hal

--- device.mk
+++ device.mk
@@ -568,7 +568,9 @@ PRODUCT_PACKAGES += \
 
 # Vibrator
 PRODUCT_PACKAGES += \
-    vendor.qti.hardware.vibrator.service
+    vendor.qti.hardware.vibrator.service \
+    android.hardware.vibrator@1.0-impl \
+    android.hardware.vibrator@1.0-service
 
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/excluded-input-devices.xml:$(TARGET_COPY_OUT_VENDOR)//etc/excluded-input-devices.xml
