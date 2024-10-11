9677bcbc5759487c28f424adf3860b98aee9a8d4
basamaryan <basam.aryan@gmail.com>
2024-10-11 17:04:38 +0200

    laurel_sprout: Switch to Xiaomi fingerprint AIDL
    Change-Id: I68dba7184034fd37454c7d32a065f041cb611625

--- device.mk
+++ device.mk
@@ -158,7 +158,7 @@ PRODUCT_PACKAGES += \
 
 # Fingerprint
 PRODUCT_PACKAGES += \
-    android.hardware.biometrics.fingerprint@2.3.vendor \
+    android.hardware.biometrics.fingerprint-service.xiaomi \
     vendor.goodix.hardware.biometrics.fingerprint@2.1.vendor
 
 PRODUCT_COPY_FILES += \
@@ -367,6 +367,7 @@ PRODUCT_PACKAGES += \
 
 PRODUCT_PACKAGES += \
     init.laurel_sprout.rc \
+    init.fingerprint.rc \
     init.msm.usb.configfs.rc \
     init.qcom.rc \
     init.qcom.power.rc \
@@ -383,6 +384,7 @@ PRODUCT_PACKAGES += \
 # Sensors
 PRODUCT_PACKAGES += \
     android.hardware.sensors-service.xiaomi-multihal \
+    sensors.xiaomi.v2 \
     android.frameworks.sensorservice@1.0.vendor
 
 PRODUCT_PACKAGES += \
