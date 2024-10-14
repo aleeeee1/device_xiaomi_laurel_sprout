4c0bf88523a16ac93dd914fd5d72abb95d848534
aleeeee1 <pace.alessandro05@protonmail.com>
2024-10-14 17:10:45 +0200

    test

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -69,7 +69,7 @@ TARGET_USES_ION := true
 TARGET_FS_CONFIG_GEN := $(DEVICE_PATH)/config.fs
 
 # Fingerprint
-TARGET_SURFACEFLINGER_UDFPS_LIB := //$(DEVICE_PATH):libudfps_extension.laurel_sprout
+TARGET_SURFACEFLINGER_UDFPS_LIB := //hardware/xiaomi:libudfps_extension.xiaomi
 TARGET_USES_FOD_ZPOS := true
 
 # FM
