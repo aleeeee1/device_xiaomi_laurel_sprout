1fdddf7a66f3cca5917fbc185b3cd6fce100ad49
aleeeee1 <pace.alessandro05@protonmail.com>
2024-10-17 20:33:45 +0000

    laurel_sprout: nuked fingerprint hal from tree
    getting ready to switch to aidl

--- device.mk
+++ device.mk
@@ -158,7 +158,6 @@ PRODUCT_PACKAGES += \
 
 # Fingerprint
 PRODUCT_PACKAGES += \
-    android.hardware.biometrics.fingerprint@2.3-service.laurel_sprout \
     android.hardware.biometrics.fingerprint@2.3.vendor \
     vendor.goodix.hardware.biometrics.fingerprint@2.1.vendor
 
