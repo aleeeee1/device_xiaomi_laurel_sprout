751203acbe207db79124688e9217c5abe4fad044
Sebastiano Barezzi <barezzisebastiano@gmail.com>
2024-10-17 21:07:21 +0000

    laurel_sprout: udfps: s/hander/handler

--- fingerprint/UdfpsHandler.cpp
+++ fingerprint/UdfpsHandler.cpp
@@ -4,7 +4,7 @@
  * SPDX-License-Identifier: Apache-2.0
  */
 
-#define LOG_TAG "UdfpsHander.laurel_sprout"
+#define LOG_TAG "UdfpsHandler.laurel_sprout"
 
 #include "UdfpsHandler.h"
 
@@ -57,7 +57,7 @@ static bool readBool(int fd) {
     return c != '0';
 }
 
-class LaurelSproutUdfpsHander : public UdfpsHandler {
+class LaurelSproutUdfpsHandler : public UdfpsHandler {
   public:
     void init(fingerprint_device_t *device) {
         mDevice = device;
@@ -117,7 +117,7 @@ class LaurelSproutUdfpsHander : public UdfpsHandler {
 };
 
 static UdfpsHandler* create() {
-    return new LaurelSproutUdfpsHander();
+    return new LaurelSproutUdfpsHandler();
 }
 
 static void destroy(UdfpsHandler* handler) {
