6e1381b8f23d361b0057a8930e71282e71c0925f
aleeeee1 <pace.alessandro05@protonmail.com>
2024-10-17 20:33:45 +0000

    laurel_sprout: nuked fingerprint hal from tree
    getting ready to switch to aidl

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -69,7 +69,6 @@ TARGET_USES_ION := true
 TARGET_FS_CONFIG_GEN := $(DEVICE_PATH)/config.fs
 
 # Fingerprint
-TARGET_SURFACEFLINGER_UDFPS_LIB := //$(DEVICE_PATH):libudfps_extension.laurel_sprout
 TARGET_USES_FOD_ZPOS := true
 
 # FM
