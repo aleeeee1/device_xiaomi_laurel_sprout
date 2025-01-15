ab2b0b7026495c43a0bd4758dee486b72a385ee0
aleeeee1 <pace.alessandro05@protonmail.com>
2025-01-15 16:44:52 +0000

    laurel_sprout: add remove-packages

--- device.mk
+++ device.mk
@@ -236,6 +236,10 @@ PRODUCT_COPY_FILES += \
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/qti_whitelist.xml:$(TARGET_COPY_OUT_SYSTEM_EXT)/etc/sysconfig/qti_whitelist.xml
 
+# RemovePackages
+PRODUCT_PACKAGES += \
+    RemovePackages
+
 # Rootdir
 PRODUCT_PACKAGES += \
     fstab.qcom
---
+++ remove-packages/Android.mk
@@ -0,0 +1,30 @@
+LOCAL_PATH := $(call my-dir)
+
+include $(CLEAR_VARS)
+LOCAL_MODULE := RemovePackages
+LOCAL_MODULE_CLASS := APPS
+LOCAL_MODULE_TAGS := optional
+LOCAL_OVERRIDES_PACKAGES := \
+    Aperture \
+    CalculatorGooglePrebuilt \
+    CalendarGooglePrebuilt \
+    Camera2 \
+    Chrome-Stub \
+    DevicePolicyPrebuilt \
+    GoogleContacts \
+    Maps \
+    Photos \
+    PixelThemesStub \
+    PixelThemesStub2022_and_newer \
+    PrebuiltGmail \
+    Snap \
+    Snap2 \
+    Velvet \
+    YouTube \
+    YouTubeMusicPrebuilt \
+    YTMusic
+
+LOCAL_UNINSTALLABLE_MODULE := true
+LOCAL_CERTIFICATE := PRESIGNED
+LOCAL_SRC_FILES := /dev/null
+include $(BUILD_PREBUILT)
