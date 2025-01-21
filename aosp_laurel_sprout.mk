9b80199b480f51e6ce3e716bbc27945390ce46dc
aleeeee1 <pace.alessandro05@protonmail.com>
2025-01-21 05:49:37 +0000

    @PixelOS-AOSP bringup

--- aosp_laurel_sprout.mk
+++ aosp_laurel_sprout.mk
@@ -8,19 +8,23 @@
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
-# Inherit some common Pixelage stuff.
-$(call inherit-product, vendor/pixelage/config/common_full_phone.mk)
+# Inherit some common PixelOS stuff.
+$(call inherit-product, vendor/aosp/config/common_full_phone.mk)
 
 # Inherit from laurel_sprout device
 $(call inherit-product, device/xiaomi/laurel_sprout/device.mk)
 
-# Pixelage props
-PIXELAGE_BUILD="laurel_sprout"
+# PixelOS props
 TARGET_FACE_UNLOCK_SUPPORTED := true
-PIXELAGE_MAINTAINER := bakkaq
+TARGET_SUPPORTS_GOOGLE_RECORDER := true
+TARGET_INCLUDE_STOCK_ARCORE := true
+TARGET_INCLUDE_STOCK_AICORE := true
+TARGET_INCLUDE_LIVE_WALLPAPERS := true
+TARGET_BOOT_ANIMATION_RES := 720
+WITH_GMS := true
 
 PRODUCT_DEVICE := laurel_sprout
-PRODUCT_NAME := pixelage_laurel_sprout
+PRODUCT_NAME := aosp_laurel_sprout
 PRODUCT_BRAND := Xiaomi
 PRODUCT_MODEL := Mi A3
 PRODUCT_MANUFACTURER := xiaomi
