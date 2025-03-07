94c9810c2eee924dbebefa3614fce34a44a5559e
Skyblueborb <tomaszborbely0710@gmail.com>
2025-03-07 20:08:19 +0100

    laurel_sprout: Cleanup compatibility matrices
    Remove DCM as it isn't needed at all.
    Remove legacy QCOM common FCM.
    Remove outdated entries from manifest.
    
    Signed-off-by: Skyblueborb <tomaszborbely0710@gmail.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -64,10 +64,8 @@ DEVICE_MANIFEST_FILE += hardware/qcom-caf/sm8150/media/conf_files/sm6150/c2_mani
 DEVICE_MANIFEST_FILE += $(DEVICE_PATH)/manifest.xml
 DEVICE_FRAMEWORK_COMPATIBILITY_MATRIX_FILE := \
     hardware/qcom-caf/common/vendor_framework_compatibility_matrix.xml \
-    hardware/qcom-caf/common/vendor_framework_compatibility_matrix_legacy.xml \
     hardware/xiaomi/vintf/xiaomi_framework_compatibility_matrix.xml \
-    vendor/aosp/config/device_framework_matrix.xml \
-    $(DEVICE_PATH)/framework_compatibility_matrix.xml
+    vendor/aosp/config/device_framework_matrix.xml
 
 # Init
 TARGET_INIT_VENDOR_LIB := //$(DEVICE_PATH):libinit_laurel_sprout
