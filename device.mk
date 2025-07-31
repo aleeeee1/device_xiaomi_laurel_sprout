ada65e8d9f3a6d0b651e17cc4fb6bc34747f69a6
Arian <arian.kulmer@web.de>
2025-07-31 16:07:41 +0200

    laurel_sprout: rootdir: Remove unnecessary class_core script
    Change-Id: I065407a7b9aaf186a22964786af97b36a140c793

--- device.mk
+++ device.mk
@@ -261,7 +261,6 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     fstab.zram \
     init.class_main.sh \
-    init.qcom.class_core.sh \
     init.qcom.early_boot.sh \
     init.qcom.post_boot.sh \
     init.qcom.sh \
