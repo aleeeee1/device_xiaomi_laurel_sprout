8860fb49d0edc8dced161311161606edfbcde763
Michael Bestas <mkbestas@lineageos.org>
2025-09-19 18:29:53 +0200

    laurel_sprout: Switch to AIDL USB HAL
    Change-Id: I70f6a4e57eef577d86987bfae44f381a4f107624

--- device.mk
+++ device.mk
@@ -269,15 +269,13 @@ PRODUCT_PACKAGES += \
     init.class_main.sh \
     init.qcom.early_boot.sh \
     init.qcom.post_boot.sh \
-    init.qcom.sh \
-    init.qcom.usb.sh
+    init.qcom.sh
 
 PRODUCT_PACKAGES += \
     init.laurel_sprout.rc \
     init.fingerprint.rc \
     init.msm.usb.configfs.rc \
     init.qcom.rc \
-    init.qcom.usb.rc \
     init.recovery.qcom.rc \
     init.target.rc \
     ueventd.qcom.rc
